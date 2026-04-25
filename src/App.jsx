6e20bc77ba6abd2695ef8c4fde5f7de52aa4a6b9
Farhan Sadik Turjo <farhansaditurjo1265@gmail.com>
2026-04-25 23:52:35 +0600

    tailwind+daisy

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <h1 className='text-4xl'>check tailwind css</h1>
+      <button className='btn btn-active'> daisy check again</button>
+    </>
+  )
+}
+
+export default App
